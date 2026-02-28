9442182193c5e72f0fd43866fe107ce2651c574d
amitchandradas2004 <amitcdas792@gmail.com>
2026-02-28 21:32:56 +0600

    Cashout is done

---
+++ script/cashout.js
@@ -0,0 +1,46 @@
+document.getElementById("cashout-btn").addEventListener("click", function () {
+  // console.log('cashout btn clicked');
+  // 1: Get the agent number and validate it
+  const cashoutNumberInput = document.getElementById("cashout-number");
+  const cashoutNumber = cashoutNumberInput.value;
+  // console.log(cashoutNumber);
+  if (cashoutNumber.length !== 11) {
+    alert("Invaild Agent Number!");
+    return;
+  }
+
+  // 2: Get the withrawal amount and convert to number
+  const cashoutAmountInput = document.getElementById("cashout-amount");
+  const cashoutAmount = cashoutAmountInput.value;
+  // console.log(cashoutAmount);
+
+  // 3: Get the current balance and convert to number
+  const balanceElement = document.getElementById("balance");
+  const balance = balanceElement.innerText;
+  // console.log(balance);
+
+  // 4: Calculate the new balance
+  const newBalance = Number(balance) - Number(cashoutAmount);
+  if (newBalance < 0) {
+    alert("Invalid Amount");
+    return;
+  }
+  console.log(newBalance);
+  // 5: Get the pin number and verify it
+  const cashoutPinInput = document.getElementById("cashout-pin");
+  const cashoutPin = cashoutPinInput.value;
+  if (cashoutPin === "1234") {
+    // 5.1: true: Show an alert > set Balance
+    alert("Cash Out Succesful");
+    console.log(cashoutPin);
+    balanceElement.innerText = newBalance;
+    cashoutNumberInput.value = "";
+    cashoutAmountInput.value = "";
+    cashoutPinInput.value = "";
+  } else {
+    // 5.2: false: Show an error alert > retuern
+    alert("Invalid Pin Number");
+    cashoutPinInput.value = "";
+    return;
+  }
+});
