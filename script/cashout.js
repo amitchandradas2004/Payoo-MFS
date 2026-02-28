3b2b7f188c6db9bdea74cc6b4f2943a920619a3b
amitchandradas2004 <amitcdas792@gmail.com>
2026-02-28 23:44:07 +0600

    Cashout is updated

--- script/cashout.js
+++ script/cashout.js
@@ -5,27 +5,29 @@ document.getElementById("cashout-btn").addEventListener("click", function () {
   const cashoutNumber = cashoutNumberInput.value;
   // console.log(cashoutNumber);
   if (cashoutNumber.length !== 11) {
-    alert("Invaild Agent Number!");
+    alert("Invaild Agent Number! Enter the valid agent number");
     return;
   }
-
+  if (cashoutNumber === "01234567890") {
+  } else {
+    alert("Invalid Agent Number");
+    return;
+  }
   // 2: Get the withrawal amount and convert to number
   const cashoutAmountInput = document.getElementById("cashout-amount");
   const cashoutAmount = cashoutAmountInput.value;
   // console.log(cashoutAmount);
-
   // 3: Get the current balance and convert to number
   const balanceElement = document.getElementById("balance");
   const balance = balanceElement.innerText;
   // console.log(balance);
-
   // 4: Calculate the new balance
   const newBalance = Number(balance) - Number(cashoutAmount);
   if (newBalance < 0) {
     alert("Invalid Amount");
     return;
   }
-  console.log(newBalance);
+  // console.log(newBalance);
   // 5: Get the pin number and verify it
   const cashoutPinInput = document.getElementById("cashout-pin");
   const cashoutPin = cashoutPinInput.value;
